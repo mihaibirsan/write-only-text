1865829995ac7a7e476eab935762112234151a71
mihaibirsan <mihaibirsan@users.noreply.github.com>
2024-09-20 18:56:01 +0000

    Deploy preview for PR 4 🛫

---
+++ pr-preview/pr-4/script.js
@@ -0,0 +1,97 @@
+(function () {
+  const params = new URLSearchParams(location.search);
+  document.documentElement.className = params.get('theme');
+  const textEl = document.getElementById('text');
+  const timeEl = document.getElementById('time');
+  const wordCountEl = document.getElementById('wordcount');
+  const cursorEl = document.getElementById('cursor');
+  
+  let totalString = window.localStorage.getItem('text') || '';
+  textEl.innerText = totalString;
+  cursorEl.value = totalString;
+  
+  let startTime = window.localStorage.getItem('startTime') || null;
+  let endTime = window.localStorage.getItem('endTime') || null;
+  
+  const currentTimeString = () => luxon.DateTime.now().set({ seconds: 0, milliseconds: 0 }).toISO({ suppressSeconds: true })
+  const wordCount = () => (totalString.match(/\w+/g) || []).length
+  
+  function commit(event) {
+    textEl.innerText = totalString;
+    window.localStorage.setItem('text', totalString);
+    event?.preventDefault();
+    cursorEl.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    
+    timeEl.innerText = `${startTime} - ${endTime}`;
+    wordCountEl.innerText = `• ${wordCount()} words`;
+  }
+  commit()
+  
+  function keydownListener(event) {
+    if (startTime === null) {
+      startTime = currentTimeString();
+      window.localStorage.setItem('startTime', startTime);
+    }
+    endTime = currentTimeString();
+    window.localStorage.setItem('endTime', endTime);
+
+    // Make sure there's input from keyboard.
+    if (document.activeElement !== cursorEl) {
+      cursorEl.focus();
+    }
+  }
+  
+  document.addEventListener('keydown', keydownListener);
+
+  // Bring up the keyboard on mobile.
+  document.addEventListener('click', () => {
+    cursorEl.focus();
+  });
+
+  // Update the text when the user types.
+  cursorEl.addEventListener('input', (event) => {
+    totalString = cursorEl.value;
+    commit(event);
+  });
+
+  // Keep the cursor at the end of the text.
+  cursorEl.addEventListener('selectionchange', (event) => {
+    cursorEl.setSelectionRange(cursorEl.value.length, cursorEl.value.length);
+  });
+
+  const copyButtonEl = document.querySelector('button#copy');
+  copyButtonEl
+    .addEventListener('click', function clearButtonClickListener(event) {
+      const copyTime = currentTimeString();
+      copy(`${totalString}\n\nStarted at:: ${startTime}\nFinished at:: ${endTime}\nCopied at:: ${copyTime}\nWord count:: ${wordCount()}`);
+      copyButtonEl.blur();
+    });
+  
+  const clearButtonEl =   document.querySelector('button#clear');
+  clearButtonEl
+    .addEventListener('click', function clearButtonClickListener(event) {
+      totalString = '';
+      startTime = null;
+      window.localStorage.removeItem('startTime');
+      endTime = null;
+      window.localStorage.removeItem('endTime');
+      commit(event);
+      clearButtonEl.blur();
+    });
+})();
+
+function copy(text) {
+  const fake = document.body.appendChild(document.createElement("textarea"));
+  fake.style.position = "absolute";
+  fake.style.left = "-9999px";
+  fake.setAttribute("readonly", "");
+  fake.value = "" + text;
+  fake.select();
+  try {
+    return document.execCommand("copy");
+  } catch (err) {
+    return false;
+  } finally {
+    fake.parentNode.removeChild(fake);
+  }
+}
