0f12c5f1af538729c991a05cb37f845756a9bb76
mihaibirsan <mihaibirsan@users.noreply.github.com>
2024-11-23 23:36:18 +0000

    Deploy preview for PR 6 🛫

---
+++ pr-preview/pr-6/README.md
@@ -0,0 +1,43 @@
+
+# Write Only Text
+
+Distraction free: write only text. Backspace, copy and clear allowed. Will tell start, end and word count for session.
+
+Originally developed at [Write Only Text (codepen.io)](https://codepen.io/mihaibirsan/full/rNWdoJj)
+
+## Features
++ Typing only appends to the end of the string. 
++ There is no editing, with one exception: backspace is permitted.
++ Text is saved to localStorage, for convenience.
+
+## TODO
++ Add friction when the "Clear" button is clicked.
++ Keep a timer.
+
+## Sounds
+
+Intended to make this a more cinematic experience, so that I can record a YouTube video with a "performance of writing". When typing especially fast, the sounds aren't appealing at all, most likely having to do with the order in which they are played.
+
+There's more work to be done for this, shelving it.
+
+Sounds used: [typewriter by yottasounds - Freesound](https://freesound.org/people/yottasounds/packs/21359/)
+
+Target experience: [typewriter22.ogg](https://freesound.org/people/tams_kp/sounds/43560/) manual typewriter some end returns
+
+## Running locally
+
+```sh
+npx http-server
+```
+
+## Development
+
+Use live reload during development.
+
+NOTE: Useful when developing for mobile!
+
+```sh
+npx browser-sync start --server --files "*.*"
+```
+
+And perhaps try [Remote debug Android devices - Chrome for Developers](https://developer.chrome.com/docs/devtools/remote-debugging/)!
---
+++ pr-preview/pr-6/script.js
@@ -0,0 +1,111 @@
+(function () {
+  const params = new URLSearchParams(location.search);
+  document.documentElement.className = params.get('theme');
+  const textEl = document.getElementById('text');
+  const timeEl = document.getElementById('time');
+  const wordCountEl = document.getElementById('wordcount');
+  const cursorEl = document.getElementById('cursor');
+
+  const typewriterSounds = Array.from({ length: 9 }, (_, i) => new Audio(`key${(i%3)+1}.wav`));
+  const playAndCycleTypewriterSound = () => {
+    typewriterSounds.unshift(typewriterSounds.pop());
+    typewriterSounds[0].play();
+  };
+  
+  let totalString = window.localStorage.getItem('text') || '';
+  textEl.innerText = totalString;
+  cursorEl.value = totalString;
+  
+  let startTime = window.localStorage.getItem('startTime') || null;
+  let endTime = window.localStorage.getItem('endTime') || null;
+  
+  const currentTimeString = () => luxon.DateTime.now().set({ milliseconds: 0 }).toISO({ suppressMilliseconds: true });
+  const timeStringToZettelID = (timeString) => timeString.replaceAll(/[- :T]+/g, '');
+  const zettelIDPretty = (zettelID) => zettelID.replace(/(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(.+)/, '<span>$1</span><span>$2</span><span>$3</span><span>$4$5</span><span>$6</span>');
+  const wordCount = () => (totalString.match(/\p{L}+/gu) || []).length
+  
+  function commit(event) {
+    textEl.innerText = totalString;
+    window.localStorage.setItem('text', totalString);
+    event?.preventDefault();
+    cursorEl.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    
+    timeEl.innerHTML = [
+      zettelIDPretty(timeStringToZettelID(startTime)),
+      '<span>-></span>',
+      zettelIDPretty(timeStringToZettelID(endTime)),
+    ].join('');
+    wordCountEl.innerText = `• ${wordCount()} words`;
+  }
+  commit()
+  
+  function keydownListener(event) {
+    if (startTime === null) {
+      startTime = currentTimeString();
+      window.localStorage.setItem('startTime', startTime);
+    }
+    endTime = currentTimeString();
+    window.localStorage.setItem('endTime', endTime);
+
+    // Make sure there's input from keyboard.
+    if (document.activeElement !== cursorEl) {
+      cursorEl.focus();
+    }
+  }
+  
+  document.addEventListener('keydown', keydownListener);
+
+  // Bring up the keyboard on mobile.
+  document.addEventListener('click', () => {
+    cursorEl.focus();
+  });
+
+  // Update the text when the user types.
+  cursorEl.addEventListener('input', (event) => {
+    totalString = cursorEl.value;
+    playAndCycleTypewriterSound();
+    setTimeout(() => commit(event), 20);
+  });
+
+  // Keep the cursor at the end of the text.
+  cursorEl.addEventListener('selectionchange', (event) => {
+    cursorEl.setSelectionRange(cursorEl.value.length, cursorEl.value.length);
+  });
+
+  const copyButtonEl = document.querySelector('button#copy');
+  copyButtonEl
+    .addEventListener('click', function clearButtonClickListener(event) {
+      const copyTime = currentTimeString();
+      copy(`${totalString}\n\nStarted at:: ${startTime}\nFinished at:: ${endTime}\nCopied at:: ${copyTime}\nWord count:: ${wordCount()}`);
+      copyButtonEl.blur();
+    });
+  
+  const clearButtonEl =   document.querySelector('button#clear');
+  clearButtonEl
+    .addEventListener('click', function clearButtonClickListener(event) {
+      totalString = '';
+      cursorEl.value = totalString;
+      startTime = null;
+      window.localStorage.removeItem('startTime');
+      endTime = null;
+      window.localStorage.removeItem('endTime');
+      commit(event);
+      clearButtonEl.blur();
+    });
+})();
+
+function copy(text) {
+  const fake = document.body.appendChild(document.createElement("textarea"));
+  fake.style.position = "absolute";
+  fake.style.left = "-9999px";
+  fake.setAttribute("readonly", "");
+  fake.value = "" + text;
+  fake.select();
+  try {
+    return document.execCommand("copy");
+  } catch (err) {
+    return false;
+  } finally {
+    fake.parentNode.removeChild(fake);
+  }
+}
